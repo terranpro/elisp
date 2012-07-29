2552d94651c22431de29e7ce011113254c5c2250
Brian Fransioli <terranpro@capp.snu.ac.kr>
2012-07-29 19:23:56 +0900

    small change for provide

--- brian-paredit.el
+++ brian-paredit.el
@@ -3,3 +3,5 @@
     (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
     (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
     (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+
+(provide 'brian-paredit)
