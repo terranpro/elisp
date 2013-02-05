e965ef6855899feb2f78d9d9e7b6ecb7201a0705
Brian Fransioli <br.fransioli@samsung.com>
2013-02-05 11:51:03 -0500

    open all .h header files in c++-mode since there are so many clowns who don't know how to make a goddam c++ header file.
    TODO: this may be removed/modified in the future ?!

--- brian-cedet-minimal.el
+++ brian-cedet-minimal.el
@@ -72,6 +72,10 @@ save the pointer marker if tag is found"
 (add-hook 'c-mode-common-hook 'my-cedet-hook)
 (add-hook 'emacs-lisp-mode-hook 'my-cedet-hook)
 (add-hook 'lisp-interaction-mode-hook 'my-cedet-hook)
-
+;; just open .h files in c++ mode since all these retard who coders
+;; don't know wtf they are doing name them garbage.h - TILT
+(setq auto-mode-alist
+      (append '(("\\.h\\'" . c++-mode))
+	      auto-mode-alist))
 
 (provide 'brian-cedet-minimal)
