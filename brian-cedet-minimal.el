de5087537003f7a668fc1848e20df67420d5bc83
Brian Fransioli <br.fransioli@samsung.com>
2013-02-05 10:44:12 -0500

    added awesome push/pop ring wrapper for semantic-ia-jump. now we can jump around and then back up !

--- brian-cedet-minimal.el
+++ brian-cedet-minimal.el
@@ -13,13 +13,50 @@
 		 (expand-file-name (concat cedet-root-path "doc/info")))
 
 
+;; amazing jump and return functions using semantic-ia-fast-jump
+(defvar semantic-tags-location-ring (make-ring 20))
+
+(defun semantic-goto-definition (point)
+  "Goto definition using semantic-ia-fast-jump   
+save the pointer marker if tag is found"
+  (interactive "d")
+  (condition-case err
+      (progn                            
+        (ring-insert semantic-tags-location-ring (point-marker))  
+        (semantic-ia-fast-jump point))
+    (error
+     ;;if not found remove the tag saved in the ring  
+     (set-marker (ring-remove semantic-tags-location-ring 0) nil nil)
+     (signal (car err) (cdr err)))))
+
+(defun semantic-pop-tag-mark ()             
+  "popup the tag save by semantic-goto-definition"   
+  (interactive)                                                    
+  (if (ring-empty-p semantic-tags-location-ring)                   
+      (message "%s" "No more tags available")                      
+    (let* ((marker (ring-remove semantic-tags-location-ring 0))    
+              (buff (marker-buffer marker))                        
+                 (pos (marker-position marker)))                   
+      (if (not buff)                                               
+            (message "Buffer has been deleted")                    
+        (switch-to-buffer buff)                                    
+        (goto-char pos))                                           
+      (set-marker marker nil nil))))
+
+;; replace garbage tags- defaults
+(global-set-key (kbd "M-.") 'semantic-goto-definition)
+(global-set-key (kbd "M-*") 'semantic-pop-tag-mark)
+
 ;; test
 (defun my-cedet-hook ()
   (local-set-key [(control return)] 'semantic-ia-complete-symbol)
   (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
   (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
   (local-set-key "\C-c=" 'semantic-decoration-include-visit)
-  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  ;replaced w/better+upgraded semantic-goto-definition
+  ;(local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  (local-set-key "\C-cj" 'semantic-goto-definition)
+  (local-set-key "\C-c\C-j" 'semantic-goto-definition)
   (local-set-key "\C-cq" 'semantic-ia-show-doc)
   (local-set-key "\C-cs" 'semantic-ia-show-summary)
   (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
