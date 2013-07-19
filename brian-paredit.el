28c63a4f958bf847160de9cebb00263135228967
Brian Fransioli <br.fransioli@samsung.com>
2013-07-19 20:51:45 +0900

    Editing Paredit settings to add colored parens matching

--- brian-paredit.el
+++ brian-paredit.el
@@ -1,7 +1,13 @@
+(require 'paren)
 (autoload 'paredit-mode "paredit"
-      "Minor mode for pseudo-structurally editing Lisp code." t)
-    (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
-    (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
-    (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+(let ((lispinit (lambda () 
+		  (paredit-mode +1)
+		  (make-variable-buffer-local 'show-paren-mode)
+		  (set (make-variable-buffer-local 'show-paren-style) 'parenthesis)
+		  (show-paren-mode 1))))
+  (add-hook 'emacs-lisp-mode-hook       lispinit)
+  (add-hook 'lisp-mode-hook             lispinit)
+  (add-hook 'lisp-interaction-mode-hook lispinit))
 
 (provide 'brian-paredit)
