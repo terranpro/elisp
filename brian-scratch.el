adb3e6ca4b05639f385dc1b7a95b03d4ed9481c4
Brian Fransioli <terranpro@capp.snu.ac.kr>
2013-08-19 22:06:37 +0900

    Fixed up buffer-mode and added support for throwing the current buffer and seeing the previous buffer (using `other-buffer') in the current window.
    Cool org-mode hack for inline images using RAW image data!

--- brian-scratch.el
+++ brian-scratch.el
@@ -674,3 +674,102 @@ settings of `c-cleanup-list' are done."
        collect (cdr prov) into features
        finally return features))
 
+
+;; Hacking org mode to display inline images 
+;; StackOverflow related: 
+;; http://stackoverflow.com/questions/17435995/paste-an-image-on-clipboard-to-emacs-org-mode-file-without-saving-it/
+(defun brian-org-find-next-inline-image-data (maxpt)
+  (let ((re-in-b (rx  "#<<<IMAGE>>>"))
+	(re-in-e (rx  "#<<</IMAGE>>>"))
+	beg end
+	dbeg dend)
+    (when (re-search-forward re-in-b maxpt t)
+      (setq beg (match-beginning 0))
+      (setq dbeg (1+ (match-end 0)))
+      (when (re-search-forward re-in-e maxpt t)
+	(setq end (match-end 0))
+	(setq dend (1- (match-beginning 0)))
+	(list beg end
+	      (string-make-unibyte
+	       (buffer-substring-no-properties dbeg dend)))))))
+
+(defun brian-org-show-inline-image-data (inl-data-l)
+"Show an inline image given INL-DATA-L which is a list of three elments: BEG END DATA , containing the beginning point of the inline image data region (including the marker/tag), "
+  (let* ((imgdata (third inl-data-l))
+	 (beg (first inl-data-l))
+	 (end (second inl-data-l))
+	 (img (create-image imgdata nil t :width 'imagemagick))
+	 (ov (make-overlay beg end)))
+    (overlay-put ov 'display img)
+    (overlay-put ov 'face 'default)
+    (overlay-put ov 'org-image-overlay t)
+    (overlay-put ov 'modification-hooks
+		 (list 'org-display-inline-remove-overlay))
+    (push ov org-inline-image-overlays)))
+
+(defun org-display-inline-images (&optional include-linked refresh beg end)
+  "Display inline images.
+Normally only links without a description part are inlined, because this
+is how it will work for export.  When INCLUDE-LINKED is set, also links
+with a description part will be inlined.  This can be nice for a quick
+look at those images, but it does not reflect what exported files will look
+like.
+When REFRESH is set, refresh existing images between BEG and END.
+This will create new image displays only if necessary.
+BEG and END default to the buffer boundaries."
+  (interactive "P")
+  (when (display-graphic-p)
+    (unless refresh
+      (org-remove-inline-images)
+      (if (fboundp 'clear-image-cache) (clear-image-cache)))
+    (save-excursion
+      (save-restriction
+	(widen)
+	(setq beg (or beg (point-min)) end (or end (point-max)))
+	(goto-char beg)
+
+	(let (next-img-data)
+	  (while (setq next-img-data (brian-org-find-next-inline-image-data end))
+	    (brian-org-show-inline-image-data next-img-data)))
+
+	(goto-char beg)
+	(let ((re (concat "\\[\\[\\(\\(file:\\)\\|\\([./~]\\)\\)\\([^]\n]+?"
+			  (substring (org-image-file-name-regexp) 0 -2)
+			  "\\)\\]" (if include-linked "" "\\]")))
+	      (case-fold-search t)
+	      
+	      old file ov img type attrwidth width)
+	  (while (and (re-search-forward re end t))
+	    (setq old (get-char-property-and-overlay (match-beginning 1)
+						     'org-image-overlay)
+		  file (expand-file-name
+			(concat (or (match-string 3) "") (match-string 4))))
+	    (when (image-type-available-p 'imagemagick)
+	      (setq attrwidth (if (or (listp org-image-actual-width)
+				      (null org-image-actual-width))
+				  (save-excursion
+				    (save-match-data
+				      (when (re-search-backward
+					     "#\\+attr.*:width[ \t]+\\([^ ]+\\)"
+					     (save-excursion
+					       (re-search-backward "^[ \t]*$\\|\\`" nil t)) t)
+					(string-to-number (match-string 1))))))
+		    width (cond ((eq org-image-actual-width t) nil)
+				((null org-image-actual-width) attrwidth)
+				((numberp org-image-actual-width)
+				 org-image-actual-width)
+				((listp org-image-actual-width)
+				 (or attrwidth (car org-image-actual-width))))
+		    type (if width 'imagemagick)))
+	    (when (file-exists-p file)
+	      (if (and (car-safe old) refresh)
+		  (image-refresh (overlay-get (cdr old) 'display))
+		(setq img (save-match-data (create-image file type nil :width width)))
+		(when img
+		  (setq ov (make-overlay (match-beginning 0) (match-end 0)))
+		  (overlay-put ov 'display img)
+		  (overlay-put ov 'face 'default)
+		  (overlay-put ov 'org-image-overlay t)
+		  (overlay-put ov 'modification-hooks
+			       (list 'org-display-inline-remove-overlay))
+		  (push ov org-inline-image-overlays))))))))))
