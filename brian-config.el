22da4e00a0df7840e654b2143014437fc7787190
Brian Fransioli <br.fransioli@samsung.com>
2013-08-21 15:52:24 +0900

    Added some config changes for vertical display in ido Also added ido-invoke which allows ido-find-file to open in another frame, window, new split hor/vert - and may work for other aspects of ido also.
    It's old and potentially buggy - requires more testing...

--- brian-config.el
+++ brian-config.el
@@ -141,6 +141,21 @@
 ;(setq ido-everywhere t)
 (ido-everywhere t)
 
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations
+      (quote ("\n-> " "" "\n   " "\n   ..." "[" "]"
+	      " [No match]" " [Matched]" " [Not readable]" " [Too big]"
+	      " [Confirm]")))
+;; Vertical completion display in ido!
+(defun ido-disable-line-truncation () 
+  (set
+   (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 ;; TODO: want faster buffer switching than C-x b
 ;; but i'm only testing this key combo
 (global-set-key (kbd "C-`") 'ido-switch-buffer)
